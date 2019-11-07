cb64230164debabe02e634df0bed013e631574f8
mohamed bardouni <mbardouni44@gmail.com>
2019-11-07 23:34:39 +0100

    تمت اضافة تطبيق الحدف و التعديل على الجدول

--- MyStore/frmBranchAdd.Designer.cs
+++ MyStore/frmBranchAdd.Designer.cs
@@ -28,20 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBranchAdd));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bfbEdit = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.bfbDelete = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.bunifuCustomDataGrid1 = new Bunifu.Framework.UI.BunifuCustomDataGrid();
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxBranch = new System.Windows.Forms.TextBox();
             this.btnSaveBranch = new System.Windows.Forms.Button();
-            this.bunifuCustomDataGrid1 = new Bunifu.Framework.UI.BunifuCustomDataGrid();
+            this.lblId = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuCustomDataGrid1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.lblId);
+            this.groupBox1.Controls.Add(this.bfbEdit);
+            this.groupBox1.Controls.Add(this.bfbDelete);
             this.groupBox1.Controls.Add(this.bunifuCustomDataGrid1);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.textBoxBranch);
@@ -55,76 +62,118 @@
             this.groupBox1.Text = "فروع الشركة";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // label1
+            // bfbEdit
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("عصري عادي", 9F);
-            this.label1.Location = new System.Drawing.Point(306, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 14);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "الفرع";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.bfbEdit.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bfbEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bfbEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bfbEdit.BorderRadius = 0;
+            this.bfbEdit.ButtonText = "تعديل";
+            this.bfbEdit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bfbEdit.DisabledColor = System.Drawing.Color.Gray;
+            this.bfbEdit.Iconcolor = System.Drawing.Color.Transparent;
+            this.bfbEdit.Iconimage = ((System.Drawing.Image)(resources.GetObject("bfbEdit.Iconimage")));
+            this.bfbEdit.Iconimage_right = null;
+            this.bfbEdit.Iconimage_right_Selected = null;
+            this.bfbEdit.Iconimage_Selected = null;
+            this.bfbEdit.IconMarginLeft = 0;
+            this.bfbEdit.IconMarginRight = 0;
+            this.bfbEdit.IconRightVisible = true;
+            this.bfbEdit.IconRightZoom = 0D;
+            this.bfbEdit.IconVisible = true;
+            this.bfbEdit.IconZoom = 90D;
+            this.bfbEdit.IsTab = false;
+            this.bfbEdit.Location = new System.Drawing.Point(116, 59);
+            this.bfbEdit.Name = "bfbEdit";
+            this.bfbEdit.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bfbEdit.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.bfbEdit.OnHoverTextColor = System.Drawing.Color.White;
+            this.bfbEdit.selected = false;
+            this.bfbEdit.Size = new System.Drawing.Size(99, 33);
+            this.bfbEdit.TabIndex = 6;
+            this.bfbEdit.Text = "تعديل";
+            this.bfbEdit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bfbEdit.Textcolor = System.Drawing.Color.White;
+            this.bfbEdit.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bfbEdit.Click += new System.EventHandler(this.bunifuFlatButton2_Click);
             // 
-            // textBoxBranch
+            // bfbDelete
             // 
-            this.textBoxBranch.Location = new System.Drawing.Point(34, 31);
-            this.textBoxBranch.Name = "textBoxBranch";
-            this.textBoxBranch.Size = new System.Drawing.Size(246, 21);
-            this.textBoxBranch.TabIndex = 1;
-            this.textBoxBranch.TextChanged += new System.EventHandler(this.textBoxBranch_TextChanged);
-            // 
-            // btnSaveBranch
-            // 
-            this.btnSaveBranch.Font = new System.Drawing.Font("عصري عادي", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnSaveBranch.Location = new System.Drawing.Point(122, 77);
-            this.btnSaveBranch.Name = "btnSaveBranch";
-            this.btnSaveBranch.Size = new System.Drawing.Size(75, 23);
-            this.btnSaveBranch.TabIndex = 0;
-            this.btnSaveBranch.Text = "اضافة";
-            this.btnSaveBranch.UseVisualStyleBackColor = true;
-            this.btnSaveBranch.Click += new System.EventHandler(this.btnSaveBranch_Click);
+            this.bfbDelete.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bfbDelete.BackColor = System.Drawing.Color.Red;
+            this.bfbDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bfbDelete.BorderRadius = 0;
+            this.bfbDelete.ButtonText = "حدف";
+            this.bfbDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bfbDelete.DisabledColor = System.Drawing.Color.Gray;
+            this.bfbDelete.Iconcolor = System.Drawing.Color.Transparent;
+            this.bfbDelete.Iconimage = ((System.Drawing.Image)(resources.GetObject("bfbDelete.Iconimage")));
+            this.bfbDelete.Iconimage_right = null;
+            this.bfbDelete.Iconimage_right_Selected = null;
+            this.bfbDelete.Iconimage_Selected = null;
+            this.bfbDelete.IconMarginLeft = 0;
+            this.bfbDelete.IconMarginRight = 0;
+            this.bfbDelete.IconRightVisible = true;
+            this.bfbDelete.IconRightZoom = 0D;
+            this.bfbDelete.IconVisible = true;
+            this.bfbDelete.IconZoom = 90D;
+            this.bfbDelete.IsTab = false;
+            this.bfbDelete.Location = new System.Drawing.Point(0, 59);
+            this.bfbDelete.Name = "bfbDelete";
+            this.bfbDelete.Normalcolor = System.Drawing.Color.Red;
+            this.bfbDelete.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.bfbDelete.OnHoverTextColor = System.Drawing.Color.White;
+            this.bfbDelete.selected = false;
+            this.bfbDelete.Size = new System.Drawing.Size(86, 33);
+            this.bfbDelete.TabIndex = 4;
+            this.bfbDelete.Text = "حدف";
+            this.bfbDelete.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bfbDelete.Textcolor = System.Drawing.Color.White;
+            this.bfbDelete.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bfbDelete.Click += new System.EventHandler(this.bunifuFlatButton1_Click_1);
             // 
             // bunifuCustomDataGrid1
             // 
             this.bunifuCustomDataGrid1.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
             this.bunifuCustomDataGrid1.AllowDrop = true;
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(209)))), ((int)(((byte)(247)))));
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("عصري عادي", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Aqua;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
-            this.bunifuCustomDataGrid1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(209)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("عصري عادي", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.Color.Aqua;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.Color.White;
+            this.bunifuCustomDataGrid1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle10;
             this.bunifuCustomDataGrid1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.bunifuCustomDataGrid1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.bunifuCustomDataGrid1.BackgroundColor = System.Drawing.Color.White;
             this.bunifuCustomDataGrid1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.bunifuCustomDataGrid1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(123)))), ((int)(((byte)(226)))));
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("عصري عادي", 9F);
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.bunifuCustomDataGrid1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.bunifuCustomDataGrid1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(123)))), ((int)(((byte)(226)))));
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("عصري عادي", 9F);
+            dataGridViewCellStyle11.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.bunifuCustomDataGrid1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle11;
             this.bunifuCustomDataGrid1.ColumnHeadersHeight = 30;
             this.bunifuCustomDataGrid1.DoubleBuffered = true;
+            this.bunifuCustomDataGrid1.EnableHeadersVisualStyles = false;
             this.bunifuCustomDataGrid1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
             this.bunifuCustomDataGrid1.HeaderBgColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(123)))), ((int)(((byte)(226)))));
             this.bunifuCustomDataGrid1.HeaderForeColor = System.Drawing.Color.White;
             this.bunifuCustomDataGrid1.ImeMode = System.Windows.Forms.ImeMode.Katakana;
-            this.bunifuCustomDataGrid1.Location = new System.Drawing.Point(0, 106);
+            this.bunifuCustomDataGrid1.Location = new System.Drawing.Point(0, 112);
             this.bunifuCustomDataGrid1.Name = "bunifuCustomDataGrid1";
             this.bunifuCustomDataGrid1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("عصري عادي", 9F);
-            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.bunifuCustomDataGrid1.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle12.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("عصري عادي", 9F);
+            dataGridViewCellStyle12.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.bunifuCustomDataGrid1.RowHeadersDefaultCellStyle = dataGridViewCellStyle12;
             this.bunifuCustomDataGrid1.RowHeadersVisible = false;
             this.bunifuCustomDataGrid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.bunifuCustomDataGrid1.Size = new System.Drawing.Size(487, 296);
@@ -133,6 +182,48 @@
             this.bunifuCustomDataGrid1.VirtualMode = true;
             this.bunifuCustomDataGrid1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.bunifuCustomDataGrid1_CellContentClick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("عصري عادي", 9F);
+            this.label1.Location = new System.Drawing.Point(450, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "الفرع";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxBranch
+            // 
+            this.textBoxBranch.Location = new System.Drawing.Point(96, 24);
+            this.textBoxBranch.Name = "textBoxBranch";
+            this.textBoxBranch.Size = new System.Drawing.Size(318, 21);
+            this.textBoxBranch.TabIndex = 1;
+            this.textBoxBranch.TextChanged += new System.EventHandler(this.textBoxBranch_TextChanged);
+            // 
+            // btnSaveBranch
+            // 
+            this.btnSaveBranch.Font = new System.Drawing.Font("عصري عادي", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnSaveBranch.Location = new System.Drawing.Point(0, 24);
+            this.btnSaveBranch.Name = "btnSaveBranch";
+            this.btnSaveBranch.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveBranch.TabIndex = 0;
+            this.btnSaveBranch.Text = "اضافة";
+            this.btnSaveBranch.UseVisualStyleBackColor = true;
+            this.btnSaveBranch.Click += new System.EventHandler(this.btnSaveBranch_Click);
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Enabled = false;
+            this.lblId.Location = new System.Drawing.Point(421, 10);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(35, 14);
+            this.lblId.TabIndex = 7;
+            this.lblId.Text = "label2";
+            this.lblId.Visible = false;
+            this.lblId.Click += new System.EventHandler(this.label2_Click);
+            // 
             // frmBranchAdd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -140,6 +231,7 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(209)))), ((int)(((byte)(247)))));
             this.ClientSize = new System.Drawing.Size(527, 450);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmBranchAdd";
             this.Text = "frmBranchAdd";
             this.Load += new System.EventHandler(this.frmBranchAdd_Load);
@@ -157,5 +249,8 @@
         private System.Windows.Forms.TextBox textBoxBranch;
         private System.Windows.Forms.Button btnSaveBranch;
         private Bunifu.Framework.UI.BunifuCustomDataGrid bunifuCustomDataGrid1;
+        private Bunifu.Framework.UI.BunifuFlatButton bfbEdit;
+        private Bunifu.Framework.UI.BunifuFlatButton bfbDelete;
+        private System.Windows.Forms.Label lblId;
     }
 }
--- MyStore/frmBranchAdd.cs
+++ MyStore/frmBranchAdd.cs
@@ -15,6 +15,7 @@ namespace MyStore
         DB db = new DB();
         public frmBranchAdd()
         {
+            
             InitializeComponent();
         }
 
@@ -37,13 +38,14 @@ namespace MyStore
 
         private void btnSaveBranch_Click(object sender, EventArgs e)
         {
+
             //تم حل مشكل داتابايز
 
             String sql = @"INSERT INTO  Branches  (branch_name) VALUES (N'{0}')";
 
-        
+
             db.excuteSql(string.Format(sql, textBoxBranch.Text));
- 
+
             MessageBox.Show("تم الحفظ");
             show();
 
@@ -51,19 +53,25 @@ namespace MyStore
 
         private void frmBranchAdd_Load(object sender, EventArgs e)
         {
+            bfbDelete.Enabled = false;
+            bfbEdit.Enabled = false;
             show();
           
         }
         //  اظهار جدول  فروع الشركات داتابايز مع اضافة الفروع الجديدة
         void show()
         {
-
+            
             String sql = @" SELECT [branch_id] ,[branch_name]  FROM [dbo].[Branches]";
+           
             DataTable dt = db.excuteDataTable(String.Format(sql));
             bunifuCustomDataGrid1.DataSource = dt;
             bunifuCustomDataGrid1.Columns[0].HeaderText="رقم الفرع";
             bunifuCustomDataGrid1.Columns[1].HeaderText = "اسم الفرع";
+
             bunifuCustomDataGrid1.Refresh();
+          
+
         }
 
         private void bunifuCustomDataGrid1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -75,15 +83,59 @@ namespace MyStore
                 int rowindex = bunifuCustomDataGrid1.SelectedCells[0].RowIndex;
                 DataGridViewRow row = this.bunifuCustomDataGrid1.Rows[rowindex];
                 string branch_id = row.Cells["branch_id"].Value.ToString();
+                lblId.Text = branch_id;
+                string s = "ss";
+             if (int.Parse(lblId.Text) > 0 && s =="ss")
+                { bfbDelete.Enabled = true;
+                    bfbEdit.Enabled = true;  }
                 string sql = @"select branch_name from branches where branch_id= '{0}'";
+                //استعمال  excuteSql
                 string branch_name = db.excuteSql(string.Format(sql, branch_id));
 
-                textBoxBranch.Text=branch_name;
-
-
-
-
+                MessageBox.Show(branch_name);
             }
+
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+
+            //تم حل مشكل داتابايز
+
+            String sql = @"INSERT INTO  Branches  (branch_name) VALUES (N'{0}')";
+
+
+            db.excuteSql(string.Format(sql, textBoxBranch.Text));
+
+            MessageBox.Show("تم الحفظ");
+            show();
+        }
+        //حدف  delete branches
+        private void bunifuFlatButton1_Click_1(object sender, EventArgs e)
+        {
+            string sql = "delete from Branches where branch_id = '{0}' "  ;
+            db.excuteSql(string.Format(sql,lblId.Text));
+            MessageBox.Show("تم الحدف بنجاح");
+            show();
+
+        }
+        //edit تعديل الفروع
+        private void bunifuFlatButton2_Click(object sender, EventArgs e)
+        {
+            string sql = "update Branches set branch_name = N'{0}' where branch_id='{1}'";
+            db.excuteSql(string.Format(sql,textBoxBranch.Text,lblId.Text)) ;
+          
+            show();
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
 
         }
     }
